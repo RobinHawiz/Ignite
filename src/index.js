8e7d199f54d31a5605bfd922cbef03b25ce413a2
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-06 23:14:08 +0100

    Setting up Redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+//Redux setup
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+//Reducers
+import gamesReducer from "./reducers/gamesReducer";
+import userReducer from "./reducers/userReducer";
+
+const store = configureStore({
+  reducer: {
+    games: gamesReducer,
+    user: userReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
