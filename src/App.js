898f402bb12ed0f2b88eca822da051f817d742c2
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-07 11:32:46 +0100

    Global styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import { popularGamesURL } from "./api";
 //Components and pages
 import Home from "./pages/Home";
+//Styles
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
