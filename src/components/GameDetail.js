fbbe9c56c02c6641b54b540070a55277c87bed79
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-08 10:49:52 +0100

    getPlatform image

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -7,6 +7,13 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 //Utils
 import { smallImage } from "../util";
+//Images
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
@@ -16,6 +23,25 @@ const GameDetail = ({ pathId }) => {
     if (element.classList.contains("shadow")) {
       document.body.style.overflow = "auto";
       navigate("/");
+    }
+  };
+  //Get platform images
+  const getPlatform = (platformName) => {
+    switch (platformName) {
+      case "PlayStation 5":
+        return playstation;
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
     }
   };
   const { screen, game, isLoading } = useSelector((state) => state.details);
@@ -33,7 +59,10 @@ const GameDetail = ({ pathId }) => {
                 <h3>Platforms</h3>
                 <Platforms>
                   {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                    ></img>
                   ))}
                 </Platforms>
               </Info>
